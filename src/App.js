94fd930597d904460ad9ff9b62823fdc104d5f3a
Nova Merida <105570301+NovaMerida@users.noreply.github.com>
2022-11-29 23:14:14 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import IncrementDecrementCounter from './components/IncrementDecrementCounter'
+import DisplayCounter from './components/DisplayCounter'
+import AddSubtractCounter from './components/AddSubtractCounter'
+
+function App(props){
+
+return(
+  <>
+    <IncrementDecrementCounter/>
+    <AddSubtractCounter/>
+    <DisplayCounter/>
+  </>
+)
+
+}
+
+export default App
